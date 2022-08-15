e14d4f62d450be4039617a94b72b6a7f9fb0a05d
sajibsaha4647 <sksaha4648@gmail.com>
2022-08-15 23:36:38 +0600

    add specifier

--- src/com/sajib/mainclass.java
+++ src/com/sajib/mainclass.java
@@ -3,5 +3,26 @@ package com.sajib;
 public class mainclass {
     public static void main(String[] args) {
         System.out.println("hellow world");
+        //scape sequence
+        System.out.println("hellow \t world");
+        System.out.println("hellow \t world");
+        String sajib = "sajib saha" ;
+
+        // date types
+        boolean sajib1 = false;
+        char r = 'a';
+        short rs = 3314;
+        int i = 424444444;
+        float ty = 10.4f;
+        double saha = 34.34;
+
+        //using specifier
+        System.out.printf("%b\n",sajib1);
+        System.out.printf("%c\n",r);
+        System.out.printf("%d\n",rs);
+        System.out.printf("%d\n",i);
+        System.out.printf("%f\n",ty);
+        System.out.printf("%f\n",saha);
+
     }
 }
