8df6e29410d356116f25b247cf7d90fefc8f1801
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-03 01:07:08 +0600

    updated

---
+++ src/com/sajib/BeatwiseOperator.java
@@ -0,0 +1,37 @@
+package com.sajib;
+
+public class BeatwiseOperator {
+
+    public static void main(String[] args) {
+
+
+
+        int a =10;
+        int b = 40;
+
+        int c;
+
+        c = a&b;
+
+        System.out.println(c);
+//
+//        c = a|b;
+//
+//        System.out.println(c);
+//
+//        c = a^b;
+//
+//        System.out.println(c);
+
+        c = a>>1; //a ar man k 3 bar vag kora base always of 2
+
+        System.out.println(c);
+
+        c= a<<1;
+        System.out.println(c);
+
+
+
+    }
+
+}
