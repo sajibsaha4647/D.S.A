774290240d979b1fd58457c06f11b8830b3bda02
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-02 13:41:29 +0600

    updated

---
+++ src/com/sajib/triangecalculation.java
@@ -0,0 +1,24 @@
+package com.sajib;
+
+import java.util.Scanner;
+
+public class triangecalculation {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double base,height,area;
+
+        System.out.println("enter base :");
+        base = input.nextDouble();
+        System.out.println("enter height :");
+        height = input.nextDouble();
+
+        area = 0.5 * base * height ;
+
+        System.out.println("area of triange" + area);
+
+
+    }
+
+}
