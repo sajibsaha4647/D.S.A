19b97219f74ce541d39c8877722e5781bce67ade
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-02 15:04:53 +0600

    updated

--- src/com/sajib/UnaryOperator.java
+++ src/com/sajib/UnaryOperator.java
@@ -4,7 +4,14 @@ public class UnaryOperator {
 
     public static void main(String[] args) {
 
+        int x = 10 ;
+        int result=2 ;
 
+//        result = +x ;
+
+        result = -x ;
+
+        System.out.println("here result : " + result);
 
     }
 
---
+++ src/com/sajib/incrementDecrement.java
@@ -0,0 +1,18 @@
+package com.sajib;
+
+public class incrementDecrement {
+
+    public static void main(String[] args) {
+
+        int x = 10 ;
+
+        int y ;
+
+        System.out.println( "result is here : "+ ++x );
+        System.out.println( "result is here : "+ x++ );
+        System.out.println( "result is here : "+ --x );
+        System.out.println( "result is here : "+ x-- );
+
+    }
+
+}
