3915d0f2ec3fc3f32778ce9d115afb5fcd86b57c
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-09 12:45:01 +0600

    updated

--- src/com/sajib/multiplicationTable.java
+++ src/com/sajib/multiplicationTable.java
@@ -9,14 +9,20 @@ public class multiplicationTable {
 
 
         Scanner input = new Scanner(System.in);
-
-        int num,result;
-
+        int num;
         System.out.println("please enter integer number :");
         num = input.nextInt();
 
-        for(int i = 1; i <= 10; i++ ){
-            System.out.println(num + "X" +i +"=" + num*i);
+        int fnum ;
+        System.out.println("please enter integer again :");
+        fnum = input.nextInt();
+
+        for(int i = num; i <= fnum; i++ ){
+           for (int j = 1; j<=10; j++){
+               System.out.println(i + "X" +j +"=" + i*j);
+
+           }
+            System.out.println("============================");
         }
 
 
