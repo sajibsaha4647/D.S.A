e266df07d8b6ff0c9a4b26e0f3a4d2e9bf7d3a79
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-09 17:43:07 +0600

    updated

---
+++ src/com/sajib/FibonacciSeries.java
@@ -0,0 +1,31 @@
+package com.sajib;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("how much number :");
+
+        int first = 0;
+        int second = 1;
+        int fibo;
+        int n;
+        n = input.nextInt() ;
+
+        System.out.print(first +" " +second);
+
+        for(int i = 3; i<=n;i++){
+
+            fibo = first+second ;
+            System.out.print(" " + fibo);
+
+            first = second ;
+            second = fibo ;
+
+        }
+
+    }
+
+}
