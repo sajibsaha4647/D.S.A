962d78a1a2920b5855136cd9696838012fd2efeb
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-12 23:56:25 +0600

    updated

---
+++ src/com/sajib/ArrayOne.java
@@ -0,0 +1,22 @@
+package com.sajib;
+
+public class ArrayOne {
+
+    public static void main(String[] args) {
+
+        //create an array in java
+        int[] number = new int[1];
+
+        number[0] = 2;
+        number[1] = 3;
+        number[2] = 5;
+        number[3] = 7;
+        number[4] = 10;
+
+        int sum = number[0] + number[1] +  number[2] +  number[3] + number[4] ;
+
+        System.out.println(sum);
+
+    }
+
+}
