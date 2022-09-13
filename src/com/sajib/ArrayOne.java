157f0e21007aae8b8ff013a620b0941d57f388b4
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-13 22:16:17 +0600

    upsated

--- src/com/sajib/ArrayOne.java
+++ src/com/sajib/ArrayOne.java
@@ -5,7 +5,7 @@ public class ArrayOne {
     public static void main(String[] args) {
 
         //create an array in java
-        int[] number = new int[1];
+        int[] number = new int[5]; //this is dimension of array
 
         number[0] = 2;
         number[1] = 3;
@@ -14,8 +14,8 @@ public class ArrayOne {
         number[4] = 10;
 
         int sum = number[0] + number[1] +  number[2] +  number[3] + number[4] ;
-
         System.out.println(sum);
+        System.out.println(number.length);
 
     }
 
---
+++ src/com/sajib/finding_sum_and_average.java
@@ -0,0 +1,28 @@
+package com.sajib;
+
+import java.util.Scanner;
+
+public class finding_sum_and_average {
+
+    public static void main(String[] args) {
+
+        double[] number = new double[5];
+        double sum = 0 ;
+
+        System.out.println("please enter 5 number:");
+
+        Scanner input = new Scanner(System.in);
+
+        for (int i= 0 ; i<5;i++){
+            number[i] = input.nextDouble();
+        }
+
+        for (int i = 0; i<5; i++){
+            sum = sum + number[i] ;
+        }
+
+        System.out.println("the sun is "+ sum);
+
+    }
+
+}
